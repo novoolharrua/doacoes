11971f0bf28fa22dfb3c8b9d0c4db7ab74cdef29
Daniel Sales <danielbps@Outlook.com>
2019-08-04 20:05:38 -0300

    ultimas alterações

--- vuejs/src/layout/InsideNavBar.vue
+++ vuejs/src/layout/InsideNavBar.vue
@@ -7,7 +7,7 @@
   >
     <div class="md-toolbar-row md-collapse-lateral">
       <div class="md-toolbar-section-start">
-        <h3 class="md-title">Novolharua</h3>
+        <h3 class="md-title">NOVOLHARUA</h3>
       </div>
       <div class="md-toolbar-section-end">
         <md-button
